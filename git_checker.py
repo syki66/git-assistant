40603e96a4d2b178090bf3d9d1246a42a244e7c1
syki66 <66syki@gmail.com>
2021-08-01 20:09:18 +0900

    add checkAuthor func

--- git_checker.py
+++ git_checker.py
@@ -21,5 +21,38 @@ def checkDateOrder(logFile):
     f.close()
     return result
 
-print(f'커밋날짜 순서체크 : {checkDateOrder("log.txt")}')
+def checkAuthorName(logFile, name):
+    '''
+    저자 이름 확인
+    '''
+    f = open(logFile, 'r')
+    result = True
+    for i in f:
+        if i.startswith('Author'):
+            if i.split()[1] != name:
+                result = False
+    f.close()
+    return result
+
+def checkAuthorEmail(logFile, email):
+    '''
+    저자 이메일 확인
+    '''
+    f = open(logFile, 'r')
+    result = True
+    for i in f:
+        if i.startswith('Author'):
+            if i.split()[2][1:-1] != email:
+                result = False
+    f.close()
+    return result
+
+logFlie = "log.txt"
+name = "username"
+email = "email@test.com"
+
+print(f'커밋날짜 순서체크 : {checkDateOrder(logFlie)}')
+print(f'커밋저자 이름체크 : {checkAuthorName(logFlie, name)}')
+print(f'커밋저자 메일체크 : {checkAuthorEmail(logFlie, email)}')
+
 input("")
